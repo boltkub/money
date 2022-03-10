d20a3ae7ba6dda207c9918cd5d9e18904aee2c01
boltkub <100109241+boltkub@users.noreply.github.com>
2022-03-10 21:21:51 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Money.in.th ความรู้เรื่องเงิน`,
     author: {
       name: `Jirawat Panketkorn`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      summary: `who lives and works in Thailand building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
